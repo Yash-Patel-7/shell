51f8f6ac398714a5c0b7cf5873f8761c7d5d4f6f
Yash Patel <117030897+Yash-Patel-7@users.noreply.github.com>
2023-07-31 12:54:21 -0400

    Initial Commit

---
+++ testSuite/F/1/BAT/myscript.sh
@@ -0,0 +1,3 @@
+echo foo > testSuite/F/1/BAT/outBAT.txt | cat
+echo foo > testSuite/F/1/BAT/outBAT.txt > baz
+cat < foo < testSuite/F/1/BAT/outBAT.txt
---
+++ testSuite/F/1/INT/myscript.sh
@@ -0,0 +1,3 @@
+echo foo > testSuite/F/1/INT/bar.txt | cat
+echo foo > testSuite/F/1/INT/bar.txt > baz
+cat < foo < testSuite/F/1/INT/bar.txt
