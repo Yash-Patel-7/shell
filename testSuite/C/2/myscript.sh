43d08eb6a0f2b823983eec25d970a9a780f8502d
Yash Patel <117030897+Yash-Patel-7@users.noreply.github.com>
2023-07-31 12:52:06 -0400

    Initial Commit

---
+++ testSuite/C/2/myscript.sh
@@ -0,0 +1,8 @@
+echo hi
+echo world
+saldlk
+echo last call failed
+sjsksks
+echo last call failed
+echo hi again
+exit
---
+++ testSuite/C/4/myscript.sh
@@ -0,0 +1,5 @@
+echo hi world
+echo before exit
+exit
+echo after exit
+echo this should not be shown
